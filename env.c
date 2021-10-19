91f545e13f93499854966dfcf167f93328d51371
Pureum Lee <dlvnfma0501@naver.com>
2021-10-19 14:46:05 +0900

    Done

--- env.c
+++ env.c
@@ -2,11 +2,6 @@
 
 void	get_env(char **envp)
 {
-	while(*envp)
-	printf("%s\n", *envp++);
-}
-int    main(int argc, char **argv, char **envp)
-{
-    get_env(envp);
-    return (0);
+	while (*envp)
+		printf ("%s\n", *envp++);
 }
