8ce47e9f6b12e3fe90faad5d6429b7cbdaa99c76
leebang54 <leebang54@khu.ac.kr>
2021-11-04 15:50:32 +0900

    in progress : translate env

--- compile_intro/translate_env.c
+++ compile_intro/translate_env.c
@@ -2,12 +2,25 @@
 
 extern t_minishell g_shell;
 
-char	*get_env_str(char *env)
+char	*get_env_str(char *str)
 {
 	t_list	*list;
-
+	char	**envp;
+	char	*result;
 	list = g_shell.export_list;
-	while ()
+
+	while(list)
+	{
+		env = ft_split(list, '=');
+		if (ft_strncmp(envp[0], str, ft_strlen(envp[0])) == 0)
+			result = ft_strdup(envp[1]);
+		else
+			result = ft_strdup("");
+		free_str(envp);
+		list = list->next;
+	}
+	return (result);
+	
 }
 
 t_list	*convert_token(t_list *token)
